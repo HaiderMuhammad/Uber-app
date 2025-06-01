11116d2a3eced14b864e9fd6b1adf1089c959cbc
haidermuhammed <haidermuhammadk99@gmail.com>
2025-06-01 19:52:37 +0300

    feat: Implemented signIn & signOut

--- Uber-App/Core/Authentication/ViewModel/AuthViewModel.swift
+++ Uber-App/Core/Authentication/ViewModel/AuthViewModel.swift
@@ -16,14 +16,36 @@ class AuthViewModel: ObservableObject {
         userSession = Auth.auth().currentUser
     }
     
+    func signIn(withEmail email: String, password: String) {
+        Auth.auth().signIn(withEmail: email.lowercased(), password: password) { result, error in
+            if let error = error {
+                print("Falied to Sign in user: \(error.localizedDescription)")
+                return
+            }
+            
+            self.userSession = result?.user
+            print("Sign in successfully \(result!.user.uid)")
+            
+        }
+    }
+    
     func registerUser(withEmail email: String, password: String, fullname: String) {
-        Auth.auth().createUser(withEmail: email, password: password) { result, error in
+        Auth.auth().createUser(withEmail: email.lowercased(), password: password) { result, error in
             if let error = error {
                 print("Failed to Sign up user: \(error.localizedDescription) ")
                 return
             }
             
-            
+            self.userSession = result?.user
+        }
+    }
+    
+    func signout() {
+        do {
+            try Auth.auth().signOut()
+            self.userSession = nil
+        } catch {
+            print("Failed to signout user: \(error.localizedDescription)")
         }
     }
 }
