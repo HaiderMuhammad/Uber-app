281b4638c6b42a40a30c6f76793818f943a65af9
haidermuhammed <haidermuhammadk99@gmail.com>
2025-06-01 19:22:38 +0300

    feat: Implemented user register with email function

--- Uber-App/Core/Authentication/ViewModel/AuthViewModel.swift
+++ Uber-App/Core/Authentication/ViewModel/AuthViewModel.swift
@@ -15,4 +15,15 @@ class AuthViewModel: ObservableObject {
     init() {
         userSession = Auth.auth().currentUser
     }
+    
+    func registerUser(withEmail email: String, password: String, fullname: String) {
+        Auth.auth().createUser(withEmail: email, password: password) { result, error in
+            if let error = error {
+                print("Failed to Sign up user: \(error.localizedDescription) ")
+                return
+            }
+            
+            
+        }
+    }
 }
