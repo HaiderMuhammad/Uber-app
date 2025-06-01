e3415f6b92e4ed6c29248b650f4052eea0d9e246
haidermuhammed <haidermuhammadk99@gmail.com>
2025-06-01 14:54:19 +0300

    feat: Implemented expected travel time

--- Uber-App/Core/LocationSearch/ViewModel/LocationSearchVM.swift
+++ Uber-App/Core/LocationSearch/ViewModel/LocationSearchVM.swift
@@ -13,7 +13,10 @@ class LocationSearchViewModel: NSObject, ObservableObject {
     
     // MARK: - Properties
     @Published var results = [MKLocalSearchCompletion]()
-    @Published var selectedLocationCoordinate: CLLocationCoordinate2D?
+    @Published var selectedUberLocation: UberLocation?
+    @Published var pickupTime: String?
+    @Published var dropOffTime: String?
+    
     private let searchCompleter = MKLocalSearchCompleter()
     var queryFragment: String = "" {
         didSet {
@@ -39,8 +42,9 @@ class LocationSearchViewModel: NSObject, ObservableObject {
                 return
             }
             guard let item = response?.mapItems.first else { return }
-            let cooredinates = item.placemark.coordinate
-            self.selectedLocationCoordinate = cooredinates
+            let cooredinate = item.placemark.coordinate
+            self.selectedUberLocation = UberLocation(title: localSearch.title,
+                                                     coordinate: cooredinate)
         }
     }
     
@@ -53,7 +57,7 @@ class LocationSearchViewModel: NSObject, ObservableObject {
     }
     
     func computeRidePrice(forType type: RideType) -> Double  {
-        guard let destCoordinate = selectedLocationCoordinate else { return 0.0 }
+        guard let destCoordinate = selectedUberLocation?.coordinate else { return 0.0 }
         guard let userCoordinate = self.userLocation else { return 0.0 }
         
         let userLocation = CLLocation(latitude: userCoordinate.latitude, longitude: userCoordinate.longitude)
@@ -61,6 +65,43 @@ class LocationSearchViewModel: NSObject, ObservableObject {
         
         let tripDistanceInMeters = userLocation.distance(from: destination)
         return type.computePrice(for: tripDistanceInMeters)
+    }
+    
+    
+    func getDestinationRoute(from userLocation: CLLocationCoordinate2D,
+                             to destination: CLLocationCoordinate2D,
+                             completion: @escaping(MKRoute) -> Void) {
+        
+        let userPlacemark = MKPlacemark(coordinate: userLocation)
+        let destPlacemark = MKPlacemark(coordinate: destination)
+        let request = MKDirections.Request()
+        request.source = MKMapItem(placemark: userPlacemark)
+        request.destination = MKMapItem(placemark: destPlacemark)
+        let directions = MKDirections(request: request)
+        
+        directions.calculate { response, error in
+            if let error = error {
+                print("Failed to get directions \(error)")
+                return
+            }
+            
+            guard let route = response?.routes.first else { return }
+            self.configurePickupAndDropOff(with: route.expectedTravelTime)
+            
+            completion(route)
+            
+        }
+        
+    }
+    
+    func configurePickupAndDropOff(with expectedTravelTime: Double) {
+         
+        let formatter = DateFormatter()
+        formatter.dateFormat = "h:mm a"
+        
+        pickupTime  = formatter.string(from: Date())
+        dropOffTime = formatter.string(from: Date() + expectedTravelTime)
+        
     }
 }
 
---
+++ Uber-App/Models/UberLocation.swift
@@ -0,0 +1,14 @@
+//
+//  UberLocation.swift
+//  Uber-App
+//
+//  Created by Haider Muhammed on 6/1/25.
+//
+
+import CoreLocation
+
+
+struct UberLocation {
+    let title: String
+    let coordinate: CLLocationCoordinate2D
+}
